420656d9430ed95a6b61a9d9535b326b50b11c2f
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-07-13 10:34:49 -0300

    wip

--- sections/ItemsList.tsx
+++ sections/ItemsList.tsx
@@ -2,8 +2,11 @@ import { Item } from "site/components/Item/index.tsx";
 
 export default function ItemsList() {
     return (
-        <div class="flex px-12 py-8">
-            <div class="flex gap-4">
+        <div class="flex px-12 py-8 flex flex-col">
+            <h2 class="text-4xl font-bold">
+                Itens
+            </h2>
+            <div class="flex gap-4 mt-4">
                 <Item
                     image="https://via.placeholder.com/150/92c952"
                     name="Item 1"
