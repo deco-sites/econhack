2ad5e72ed6421c43ab473a230f500a61ba36799b
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-07-13 01:55:59 -0300

    Revert "Release eduardo.rodrigues environment with 0 changes made by "
    This reverts commit b4734e81facc5b4d7a0846f4dd6cf199c1dd67db.

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -3,8 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
-import * as $$$$$$$$$$$1 from "./apps/deco/records.ts";
-import * as $$$$$$$$$$$2 from "./apps/site.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Footer.tsx";
 import * as $$$$$$1 from "./sections/ItWorks.tsx";
 import * as $$$$$$2 from "./sections/Theme/Theme.tsx";
@@ -19,8 +18,7 @@ const manifest = {
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
-    "site/apps/deco/records.ts": $$$$$$$$$$$1,
-    "site/apps/site.ts": $$$$$$$$$$$2,
+    "site/apps/site.ts": $$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
