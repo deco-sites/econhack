efca7a4ac1ad8a9f9fb8690f7c2e85024c1a4f74
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-07-13 22:45:25 -0300

    active search

--- loaders/vtexProductSearch.ts
+++ loaders/vtexProductSearch.ts
@@ -2,11 +2,15 @@ import { Product } from "apps/vtex/utils/types.ts";
 
 export interface Props {
   domain: string;
-  queryString: string;
+  query: Record<string, string>;
 }
 
 export default async function loader(props: Props): Promise<Product[]> {
+  const queryString = new URLSearchParams(props.query).toString();
+
+  console.log({ queryString })
+
   return await fetch(
-    `${props.domain}/api/catalog_system/pub/products/search?${props.queryString}`,
+    `${props.domain}/api/catalog_system/pub/products/search?${queryString}`,
   ).then((res) => res.json());
 }
--- sections/FeaturedProducts.tsx
+++ sections/FeaturedProducts.tsx
@@ -31,8 +31,34 @@ export interface Props {
   };
 }
 
-export async function loader(props: Props, _req: Request, ctx: AppContext) {
+export async function loader(props: Props, req: Request, ctx: AppContext) {
   const state = props.state ?? { storeIdx: 0 };
+
+  if (req.headers.get("content-type") === "application/x-www-form-urlencoded") {
+    const ft = await req.formData().then((d) => d.get("search")) as string;
+
+    let res = await ctx.invoke.site.loaders.vtexProductSearch({
+      domain: props.stores[state.storeIdx ?? 0].url,
+      query: { ft },
+    });
+
+    res = res.splice(0, 8);
+
+    const products = await Promise.all(res.map(async (r) => {
+      const item = toItem(r, r.items[0]);
+
+      const inList = !!(await ctx.invoke.site.loaders.item({
+        id: item.id,
+      }));
+
+      return {
+        item,
+        inList,
+      };
+    }));
+
+    return { ...props, state: { storeIdx: state.storeIdx, products } };
+  }
 
   if (state.idx && state.products?.length) {
     const product = state.products[state.idx];
@@ -87,7 +113,9 @@ export default function Section(props: SectionProps<typeof loader>) {
       style={store.theme ? { backgroundColor: store.theme } : {}}
     >
       <div class="flex flex-col items-center text-white mt-8 mb-8">
-        <h1 class="text-3xl font-extrabold uppercase">Monte sua lista de presente</h1>
+        <h1 class="text-3xl font-extrabold uppercase">
+          Monte sua lista de presente
+        </h1>
         <p class="text-xs">
           Selecione uma de nossas lojas parceiras e busque os produtos que
           deseja ser presenteado(a).
@@ -117,70 +145,81 @@ export default function Section(props: SectionProps<typeof loader>) {
           </li>
         ))}
       </ul>
-      <ul
-        class="gap-3 mx-auto grid grid-cols-4 w-max bg-base-200 pt-6 px-6 pb-8 rounded-t-xl"
-        id={id}
-      >
-        {/* Skeleton */}
-        {[...Array(8)].map(() => (
-          <div class="skeleton w-48 h-[300px] rounded bg-gray-100 hidden [.htmx-request_&]:block">
-          </div>
-        ))}
-        {/* Content */}
-        {state.products?.map((product, idx) => (
-          <li class="relative flex flex-col w-48 p-3 border border-gray-200 rounded [.htmx-request_&]:hidden">
-            {product.item.image
-              ? (
-                <a href={product.item.url} target="_blank">
-                  <Image
-                    src={product.item.image ?? ""}
-                    width={248}
-                    height={248}
-                    class="mx-auto mb-3"
-                  />
-                </a>
-              )
-              : null}
-            <span class="text-ellipsis whitespace-nowrap text-left overflow-hidden text-xs">
-              {product.item.name}
-            </span>
-            <p class="font-bold text-sm">
-              {product.item.price.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </p>
-            <button
-              class="btn btn-secondary mt-3"
-              hx-post={useSection<typeof Section>({
-                props: { ...props, state: { ...state, idx } },
-              })}
-              hx-trigger="click"
-              hx-target="closest section"
-              hx-swap="outerHTML"
-              disabled={product.inList}
-            >
-              <span class="hidden loading loading-spinner loading-xs [.htmx-request_&]:inline " />
-              <span class="[.html-request_&]:hidden inline">
-                {product.inList ? "Adicionado" : "Adicionar à lista"}
+      <div class="w-max bg-base-200 pt-6 px-6 pb-8 rounded-t-xl mx-auto">
+        <input
+          class="input input-bordered"
+          name="search"
+          type="search"
+          placeholder={`Busque em ${store.name}`}
+          hx-post={useSection<typeof Section>({ props })}
+          hx-trigger="input changed delay:500ms, search"
+          hx-target="closest section"
+        />
+        <ul
+          class="gap-3 grid grid-cols-4"
+          id={id}
+        >
+          {/* Skeleton */}
+          {[...Array(8)].map(() => (
+            <div class="skeleton w-48 h-[300px] rounded bg-gray-100 hidden [.htmx-request_&]:block">
+            </div>
+          ))}
+          {/* Content */}
+          {state.products?.map((product, idx) => (
+            <li class="relative flex flex-col w-48 p-3 border border-gray-200 rounded [.htmx-request_&]:hidden">
+              {product.item.image
+                ? (
+                  <a href={product.item.url} target="_blank">
+                    <Image
+                      src={product.item.image ?? ""}
+                      width={248}
+                      height={248}
+                      class="mx-auto mb-3"
+                    />
+                  </a>
+                )
+                : null}
+              <span class="text-ellipsis whitespace-nowrap text-left overflow-hidden text-xs">
+                {product.item.name}
               </span>
-            </button>
-            {product.inList && (
+              <p class="font-bold text-sm">
+                {product.item.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
               <button
-                class="absolute top-0 right-0 p-1 bg-white rounded-full shadow-black"
+                class="btn btn-secondary mt-3"
                 hx-post={useSection<typeof Section>({
                   props: { ...props, state: { ...state, idx } },
                 })}
                 hx-trigger="click"
                 hx-target="closest section"
                 hx-swap="outerHTML"
+                disabled={product.inList}
               >
-                <Icon id="Trash" width={20} height={20} />
+                <span class="hidden loading loading-spinner loading-xs [.htmx-request_&]:inline " />
+                <span class="[.html-request_&]:hidden inline">
+                  {product.inList ? "Adicionado" : "Adicionar à lista"}
+                </span>
               </button>
-            )}
-          </li>
-        ))}
-      </ul>
+              {product.inList && (
+                <button
+                  class="absolute top-0 right-0 p-1 bg-white rounded-full shadow-black"
+                  hx-post={useSection<typeof Section>({
+                    props: { ...props, state: { ...state, idx } },
+                  })}
+                  hx-trigger="click"
+                  hx-target="closest section"
+                  hx-swap="outerHTML"
+                >
+                  <Icon id="Trash" width={20} height={20} />
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
