3ba4325ae5b43173fc924f6122f95df8d1e11008
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-07-14 10:39:55 -0300

    wip

--- sections/Invitations.tsx
+++ sections/Invitations.tsx
@@ -1,11 +1,15 @@
-import { Invitee } from "site/loaders/invitees.ts";
 import { AppContext } from "site/apps/site.ts";
 import CopyInviteeLink from "site/islands/CopyInviteeLink.tsx";
 import Icon from "site/components/ui/Icon.tsx";
 import { useSection } from "deco/hooks/useSection.ts";
+import { InviteeAndGift } from "site/loaders/invitees-and-gifts.ts";
+import Modal from "site/components/ui/Modal.tsx";
+import Header from "site/sections/Header.tsx";
+import Image from "apps/website/components/Image.tsx";
 
 export interface Props {
-  invitees: Invitee[];
+  invitees: InviteeAndGift[];
+  openList?: string;
 }
 
 export function loader(props: Props, req: Request, _ctx: AppContext) {
@@ -22,7 +26,7 @@ export function loader(props: Props, req: Request, _ctx: AppContext) {
   return props;
 }
 
-export default function Section(props: Props) {
+export default function InvitationsSection(props: Props) {
   return (
     <div class="bg-primary min-h-screen flex flex-col justify-end transition-colors">
       <h3 class="text-3xl font-extrabold uppercase text-center text-white mb-8">
@@ -33,19 +37,79 @@ export default function Section(props: Props) {
           {props.invitees.map((invitee) => {
             return (
               <div class="h-14 border-b border-gray-200 flex items-center justify-between">
-                <span class="font-bold w-[20%] flex">
-                  {invitee.username}
-                </span>
+                <div class="flex gap-4">
+                  <span class="font-bold flex">
+                    {invitee.user.username}
+                  </span>
+
+                  <button
+                    class="cursor-pointer"
+                    hx-swap="outerHTML"
+                    hx-target="closest section"
+                    hx-get={useSection<Props>({
+                      props: { openList: invitee.user.id },
+                    })}
+                  >
+                    Exibir
+                  </button>
+                  <Modal open={props.openList === invitee.user.id}>
+                    <div class="bg-white rounded p-4 relative max-h-[90vh] overflow-y-auto">
+                      <button
+                        class="absolute top-2 right-2"
+                        hx-get={useSection<typeof Header>({
+                          props: { openList: false },
+                        })}
+                        hx-target="closest section"
+                        hx-swap="outerHTML"
+                      >
+                        <Icon
+                          id="XMark"
+                          width={24}
+                          height={24}
+                          strokeWidth={2}
+                        />
+                      </button>
+                      <ul>
+                        {invitee.items.map((item) => (
+                          <li>
+                            {item.image
+                              ? (
+                                <a href={item.url} target="_blank">
+                                  <Image
+                                    src={item.image ?? ""}
+                                    width={248}
+                                    height={248}
+                                    class="mx-auto mb-3"
+                                  />
+                                </a>
+                              )
+                              : null}
+                            <span class="text-ellipsis whitespace-nowrap text-left overflow-hidden text-xs">
+                              {item.name}
+                            </span>
+
+                            <p class="font-bold text-sm">
+                              {item.price.toLocaleString("pt-BR", {
+                                style: "currency",
+                                currency: "BRL",
+                              })}
+                            </p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </Modal>
+                </div>
                 <div class="flex items-center gap-3">
                   <CopyInviteeLink
-                    userId={invitee.id}
+                    userId={invitee.user.id}
                   />
                   <button
                     hx-target="closest section"
                     hx-swap="outerHTML"
                     hx-delete={useSection<Props>({
                       props,
-                      href: "?id=" + invitee.id,
+                      href: "?id=" + invitee.user.id,
                     })}
                     class="w-6 h-6 flex items-center justify-center [.htmx-request_&]:disabled"
                   >
