b4eda69c49c90d6cff52d666ab96d0f73b90aac9
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-07-14 03:06:38 -0300

    add placeholder actions

--- sections/Invitations.tsx
+++ sections/Invitations.tsx
@@ -9,6 +9,11 @@ export interface Props {
 }
 
 export function loader(props: Props, req: Request, _ctx: AppContext) {
+  if (req.method === "POST") {
+    // TODO: Implement create invitee
+    return props;
+  }
+
   if (req.method === "DELETE") {
     // TODO: Implement delete invitee
     return props;
@@ -24,9 +29,6 @@ export default function Section(props: Props) {
         Convidados
       </h3>
       <div class="w-max pt-6 px-6 pb-8 rounded-t-xl mx-auto min-w-[60vw] min-h-[70vh] bg-white">
-        <form>
-          <input type="text" placeholder="Adicionar" />
-        </form>
         <div class="flex flex-col">
           {props.invitees.map((invitee) => {
             return (
@@ -45,7 +47,7 @@ export default function Section(props: Props) {
                       props,
                       href: "?id=" + invitee.id,
                     })}
-                    class="w-6 h-6 flex items-center justify-center"
+                    class="w-6 h-6 flex items-center justify-center [.htmx-request_&]:disabled"
                   >
                     <Icon
                       id="Trash"
@@ -61,6 +63,25 @@ export default function Section(props: Props) {
             );
           })}
         </div>
+        <h3 class="font-extrabold text-center mt-8">
+          Faltando alguém?
+        </h3>
+        <form
+          hx-post="/invitees"
+          hx-target="closest section"
+          hx-swap="outerHTML"
+          class="flex gap-3 mx-auto justify-center mt-3"
+        >
+          <input type="text" placeholder="Nome" class="input input-bordered" />
+          <button
+            type="submit"
+            class="btn btn-primary w-24 [.htmx-request_&]:disabled"
+          >
+            <span class="inline [.htmx-request_&]:hidden">Convidar</span>
+            <span class="w-4 h-4 loading loading-spinner hidden [.htmx-request_&]:inline">
+            </span>
+          </button>
+        </form>
       </div>
     </div>
   );
