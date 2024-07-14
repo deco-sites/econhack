5714a4d6e5392bcd12d6cadde4e6f8bd0db685a2
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-07-14 02:58:42 -0300

    add fake delete button

--- sections/Invitations.tsx
+++ sections/Invitations.tsx
@@ -1,11 +1,23 @@
 import { Invitee } from "site/loaders/invitees.ts";
+import { AppContext } from "site/apps/site.ts";
 import CopyInviteeLink from "site/islands/CopyInviteeLink.tsx";
+import Icon from "site/components/ui/Icon.tsx";
+import { useSection } from "deco/hooks/useSection.ts";
 
 export interface Props {
   invitees: Invitee[];
 }
 
-export default function Section({ invitees }: Props) {
+export function loader(props: Props, req: Request, _ctx: AppContext) {
+  if (req.method === "DELETE") {
+    // TODO: Implement delete invitee
+    return props;
+  }
+
+  return props;
+}
+
+export default function Section(props: Props) {
   return (
     <div class="bg-primary min-h-screen flex flex-col justify-end transition-colors">
       <h3 class="text-3xl font-extrabold uppercase text-center text-white mb-8">
@@ -16,15 +28,35 @@ export default function Section({ invitees }: Props) {
           <input type="text" placeholder="Adicionar" />
         </form>
         <div class="flex flex-col">
-          {invitees.map((invitee) => {
+          {props.invitees.map((invitee) => {
             return (
               <div class="h-14 border-b border-gray-200 flex items-center justify-between">
                 <span class="font-bold w-[20%] flex">
                   {invitee.username}
                 </span>
-                <CopyInviteeLink
-                  userId={invitee.id}
-                />
+                <div class="flex items-center gap-3">
+                  <CopyInviteeLink
+                    userId={invitee.id}
+                  />
+                  <button
+                    hx-target="closest section"
+                    hx-swap="outerHTML"
+                    hx-delete={useSection<Props>({
+                      props,
+                      href: "?id=" + invitee.id,
+                    })}
+                    class="w-6 h-6 flex items-center justify-center"
+                  >
+                    <Icon
+                      id="Trash"
+                      width={24}
+                      height={24}
+                      class="inline [.htmx-request_&]:hidden"
+                    />
+                    <span class="w-4 h-4 loading loading-spinner hidden [.htmx-request_&]:inline">
+                    </span>
+                  </button>
+                </div>
               </div>
             );
           })}
